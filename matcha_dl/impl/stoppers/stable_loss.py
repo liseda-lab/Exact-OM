eb394391495af0686c79ea7de0fd4b523f95e637
PedroCotovio <pgcotovio@gmail.com>
2025-03-02 20:22:55 +0000

    debug stable loss stopper types

--- matcha_dl/impl/stoppers/stable_loss.py
+++ matcha_dl/impl/stoppers/stable_loss.py
@@ -10,8 +10,8 @@ class StableLossStopping(IStopper):
             patience (int): Number of epochs to wait after the last improvement before stopping.
             min_delta (float): Minimum change in validation loss to be considered as improvement.
         """
-        self.patience = patience
-        self.min_delta = min_delta
+        self.patience = int(patience)
+        self.min_delta = float(min_delta)
         self.best_loss = float('inf')
         self.counter = 0
         self.early_stop = False
