c6fca3259204c741e35436c6a5ce826a31ea2d6c
PedroCotovio <pgcotovio@gmail.com>
2025-03-20 13:57:23 +0000

    debug ontology parser

--- matcha_dl/core/entities/ontology.py
+++ matcha_dl/core/entities/ontology.py
@@ -1,13 +1,23 @@
 
-from typing import List, Tuple, Optional
+from typing import List, Optional, Union
 
-from mowl.owlapi import OWLOntology, OWLClass, OWLAnnotationProperty, IRI, OWLReasoner
-from mowl.owlapi import OWLReasonerFactory, OWLManager
+from mowl.owlapi import OWLOntology
+
+from org.semanticweb.owlapi.search import EntitySearcher
+
+from org.semanticweb.HermiT import Reasoner
+from org.semanticweb.owlapi.model import (
+    IRI,
+    OWLAnnotationProperty,
+    OWLClass,
+)
+
+
 
 class Entity:
-    def __init__(self, class_iri: str, ontology: OWLOntology, reasoner: OWLReasoner) -> None:
+    def __init__(self, class_iri: str, ontology: OWLOntology, reasoner: Reasoner) -> None:
 
-        self._owl_class = self._get_owl_class(class_iri)
+        self._owl_class = self._get_owl_class(class_iri, ontology)
         self.ontology = ontology
         self.reasoner = reasoner
         self._labels: Optional[List[str]] = None
@@ -25,35 +35,34 @@ class Entity:
 
     @property
     def name(self) -> str:
-        return self._owlclass.getIRI().getShortForm()
+        return self._owl_class.getIRI().getShortForm()
     
     @property
     def class_iri(self) -> str:
-        return self._owlclass.getIRI().toString()
+        return self._owl_class.getIRI().toString()
 
     @property
     def labels(self) -> List[str]:
         if self._labels is None:
-            self._labels = [annotation.getValue().getLiteral() for annotation in self.owl_class.getAnnotations(self.ontology)
-                            if isinstance(annotation.getProperty(), OWLAnnotationProperty) and annotation.getProperty().isLabel()]
+            self._labels = self._extract_labels(self.owl_class, self.ontology)
         return self._labels
 
     @property
     def subclasses(self) -> List[str]:
         if self._subclasses is None:
-            self._subclasses = [subcls.getIRI().getShortForm() for subcls in self.reasoner.getSubClasses(self.owl_class, True).getFlattened() if not subcls.isOWLNothing()]
+            self._subclasses = [self._extract_labels(subcls.getIRI(), self.ontology)[0] for subcls in self.reasoner.getSubClasses(self.owl_class, True).getFlattened() if not subcls.isOWLNothing()]
         return self._subclasses
 
     @property
     def superclasses(self) -> List[str]:
         if self._superclasses is None:
-            self._superclasses = [supercls.getIRI().getShortForm() for supercls in self.reasoner.getSuperClasses(self.owl_class, True).getFlattened() if not supercls.isOWLThing()]
+            self._superclasses = [self._extract_labels(supercls.getIRI(), self.ontology)[0] for supercls in self.reasoner.getSuperClasses(self.owl_class, True).getFlattened() if not supercls.isOWLThing()]
         return self._superclasses
 
     @property
     def top_superclass(self) -> List[str]:
         if self._top_superclass is None:
-            self._top_superclass = self._find_top_superclass()
+            self._top_superclass = self._extract_labels(self._find_top_superclass(), self.ontology)[0]
         return [self._top_superclass]
 
     def _find_top_superclass(self) -> str:
@@ -64,14 +73,23 @@ class Entity:
             if not valid_superclasses:
                 break
             owl_class = valid_superclasses[0]
-        return owl_class.getIRI().getShortForm()
+        return owl_class.getIRI()
 
     @staticmethod
-    def _get_owl_class(class_iri) -> OWLClass:
-        data_factory = OWLManager.getOWLDataFactory()
-        return data_factory.getOWLClass(IRI.create(class_iri))
+    def _get_owl_class(class_iri: Union[IRI, str], ontology: OWLOntology) -> OWLClass:
+        if isinstance(class_iri, str):
+            class_iri = IRI.create(class_iri)
+        factory = ontology.getOWLOntologyManager().getOWLDataFactory()
+        return factory.getOWLClass(class_iri)
+    
+    @classmethod
+    def _extract_labels(cls, owl_class: Union[OWLClass, str], ontology: OWLOntology) -> List[str]:
+        if isinstance(owl_class, str):
+            owl_class = cls._get_owl_class(owl_class, ontology)
+        label_property = ontology.getOWLOntologyManager().getOWLDataFactory().getRDFSLabel()
+        return [annotation.getValue().asLiteral().get().getLiteral() for annotation in EntitySearcher.getAnnotations(owl_class, ontology, label_property) if annotation.getValue().isLiteral()]
 
     @classmethod
     def load_from_list(cls, class_iris: List[str], ontology: OWLOntology) -> List['Entity']:
-        reasoner = OWLReasonerFactory().createReasoner(ontology)
+        reasoner = Reasoner.ReasonerFactory().createReasoner(ontology)
         return [cls(class_iri=class_iri, ontology=ontology, reasoner=reasoner) for class_iri in class_iris]
