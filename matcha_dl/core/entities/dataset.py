d8799bc1fb2b1ce7f0c4bdefdf6f56be9869596b
PedroCotovio <pgcotovio@gmail.com>
2025-03-23 00:47:03 +0000

    refactor static propts into more modular variables, add uncertainty, remove int conf

--- matcha_dl/core/entities/dataset.py
+++ matcha_dl/core/entities/dataset.py
@@ -5,19 +5,22 @@ from matcha_dl.core.entities.configs.dataset import Likelihood
 class StaticPrompts:
     POSITIVE_SOLUTION = "yes"
     NEGATIVE_SOLUTION = "no"
+    UNCERTAIN_SOLUTION = "uncertain"
+    POSITIVE_CONFIDENCE = "confident"
+    NEGATIVE_CONFIDENCE = "not confident"
+    VERY_POSITIVE_CONFIDENCE = "very confident"
     TASK_CONTEXT = "You are doing an ontology alignment task, "
     INSTRUCTION = f"I am going to ask you a question and you should answer '{POSITIVE_SOLUTION}' or '{NEGATIVE_SOLUTION}'."
     FIRST_EXAMPLE = "For example"
     FOLLOWING_EXAMPLE = "Another example"
-    EXAMPLE_BASE = "$START given the question $EXAMPLE you should respond $SOLUTION. "
+    EXAMPLE_BASE = "$START given the question '$EXAMPLE' you should respond '$SOLUTION'. "
     SKELETON = "$TC$I$E$CONF: 'Are '$S' $CTX_S and '$T' $CTX_T $TYPE?'"
-    EXAMPLE = "Are $S and $T equivalent?"
+    EXAMPLE = "Are '$S' and '$T' equivalent?"
     CONFIDENCE = {
         Likelihood.float: f"Followed by your confidence in your answer as a score from 0 to 1, like this: '{POSITIVE_SOLUTION}:0.8'",
-        Likelihood.int: f"Followed by your confidence in your answer as a score from 0 to 10, like this: '{POSITIVE_SOLUTION}:8'",
-        Likelihood.cat: f"Followed by your confidence in your answer 'not confident', 'confident', 'very confident', like this: '{POSITIVE_SOLUTION}:confident'"
+        Likelihood.cat: f"Followed by your confidence in your answer '{NEGATIVE_CONFIDENCE}', '{POSITIVE_CONFIDENCE}', '{VERY_POSITIVE_CONFIDENCE}', like this: '{POSITIVE_SOLUTION}:confident'"
     }
-    CRITIC_SKELETON = f"You are verifying an ontology aligmnent task, given the question $P the response was $R is this correct? You should answer '{POSITIVE_SOLUTION}' or '{NEGATIVE_SOLUTION}'."
+    CRITIC_SKELETON = f"You are verifying an ontology aligmnent task, given the question $P the response was $R is this correct? You should answer '{POSITIVE_SOLUTION}' or '{NEGATIVE_SOLUTION}'$U."
 
     @classmethod
     def get_task_context(cls, task_context:bool) -> str:
@@ -34,6 +37,13 @@ class StaticPrompts:
     @classmethod
     def get_confidence(cls, likelihood: Likelihood) -> str:
         return cls.CONFIDENCE.get(likelihood, "")
+    
+    @classmethod
+    def get_critic(cls, prompt: str, response: str, uncertainty: bool = False) -> str:
+        critic = cls.CRITIC_SKELETON.replace("$P", prompt).replace("$R", response)
+        if uncertainty:
+            return critic.replace("$U", f" or {cls.UNCERTAIN_SOLUTION}")
+        return cls.CRITIC_SKELETON.replace("$U", "")
     
 
 class DatasetMask(str, Enum):
